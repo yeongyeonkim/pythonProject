9293e66c9a7eae0781664a0d5dceb65ef4c08840
YeongYeonKim <dudtbd111@naver.com>
2020-08-13 17:10:21 +0900

    Add : module.py / import, as, tuple

---
+++ module.py
@@ -0,0 +1,25 @@
+import datetime
+import math
+import math as m
+from math import pi
+from math import *
+
+now = datetime.datetime.now()
+print(now)
+
+print("The value of pi is ", math.pi)
+
+print("The value of pi is ", m.pi)
+
+print("The value of pi is ", pi)
+
+print("The value of e is ", e)
+
+str = '가나다라마바가가'
+print(len(str))
+
+print(str.count('가'))
+# .count(지정문자열) 문자열에서 지정 문자열의 개수를 세어준다.
+
+tuple = '회원', ('영연', '영은'), (28, 27), (True, True)
+print(tuple)
